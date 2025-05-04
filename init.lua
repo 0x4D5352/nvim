4ed44432eb133e11b1d757feb6a11f7de144a0db
0x4D5352 <67082011+0x4D5352@users.noreply.github.com>
2025-05-04 16:44:37 -0500

    add pico8 stuff

--- init.lua
+++ init.lua
@@ -1034,5 +1034,18 @@ vim.api.nvim_create_autocmd('VimLeave', {
 -- adjust conceallevel to work with obsidian.nvim -- 0x4D5352
 vim.opt.conceallevel = 1
 
+-- get pico8-ls working
+vim.api.nvim_create_autocmd({ 'BufNew', 'BufEnter' }, {
+  pattern = { '*.p8' },
+  callback = function(args)
+    vim.lsp.start {
+      name = 'pico8-ls',
+      cmd = { 'pico8-ls', '--stdio' },
+      root_dir = vim.fs.dirname(vim.api.nvim_buf_get_name(args.buf)),
+      -- Setup your keybinds in the on_attach function
+      on_attach = on_attach,
+    }
+  end,
+})
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
